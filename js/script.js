299432f1f86b4b335e74f2fc7f5d1a1a06960c6d
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-12-13 22:04:14 -0500

    "Provide functionality to activities event handler to disable unchecked activities with same date/time as the checked. Create deconstructed objects for the loop in the activities event handler. Add payment event handler."

--- js/script.js
+++ js/script.js
@@ -9,12 +9,11 @@ otherJobRole.hidden = true;
 const titleSelection = document.querySelector("#title");
 titleSelection.addEventListener("change", event => {
     // If user selects the "other" option only, display the text field
-    if (event.target.value === "other") {
-        otherJobRole.hidden = false;
-    }
-    else {
+    if (event.target.value !== "other") {
         otherJobRole.hidden = true;
+        return;
     }
+    otherJobRole.hidden = false;
 });
 // ============== //
 //  T-Shirt Info  //
@@ -29,7 +28,7 @@ const colorOptions = colorSelection.querySelectorAll("option");
 // Grab the Design selection menu
 const designSelection = document.querySelector("#design");
 designSelection.addEventListener("change", event => {
-    const target = event.target;
+    const { target } = event;
     // On any change, enable the Color selection menu
     colorSelection.disabled = false;
     colorOptions.forEach(color => {
@@ -43,16 +42,63 @@ designSelection.addEventListener("change", event => {
 //  Register for Activities  //
 //         Section           //
 // ========================= //
+// When the Activites selections are checked or unchecked
 document.querySelector("#activities").addEventListener("change", function () {
     // Collect the list of activities by their input element
     const activities = this.querySelectorAll(".activities-box input");
     // Grab element that displays total cost
     const totalCostDisplay = this.querySelector("#activities-cost");
+    // Declare a variable to store total cost
     let totalCost = 0;
     activities.forEach((activity) => {
-        if (activity.checked) {
-            totalCost += parseInt(activity.dataset.cost);
+        // Quick access to current activity's data attributes
+        const { dayAndTime, cost } = activity.dataset;
+        // Quick access to current activity's main attributes
+        const { checked, disabled } = activity;
+        // Function to change disable an activity depending of inputs with same date/time
+        function changeActivityDisable(activities, bool) {
+            activities.forEach((similarActivity) => {
+                if (similarActivity !== activity && similarActivity.dataset.dayAndTime === dayAndTime) {
+                    similarActivity.disabled = bool;
+                    return;
+                }
+            });
+        }
+        if (checked) {
+            // If the activity is check, add its cost to the total cost
+            totalCost += parseInt(cost);
+            // Disable activities with same date/time
+            changeActivityDisable(activities, true);
+        }
+        // If the current activity is not checked or disabled
+        if (!checked && !disabled) {
+            // Enable any activities with same date/time
+            changeActivityDisable(activities, false);
         }
     });
     totalCostDisplay.textContent = `Total: $${totalCost}`;
+});
+// ============== //
+//  Payment Info  //
+//     Section    //
+// ============== //
+// Grab the Credit Card, Paypal and Bitcoin displays
+const creditcardDisplay = document.querySelector("#credit-card");
+const paypalDisplay = document.querySelector("#paypal");
+const bitcoinDisplay = document.querySelector("#bitcoin");
+const paymentDisplays = [creditcardDisplay, paypalDisplay, bitcoinDisplay];
+// Auto Select the "Credit Card" option and hide the PayPal and Bitcoin info on page load
+document.querySelector("option[value='credit-card']").selected = true;
+paypalDisplay.hidden = true;
+bitcoinDisplay.hidden = true;
+// Check for changes on the payment method
+document.querySelector("#payment").addEventListener("change", event => {
+    const target = event.target;
+    paymentDisplays.forEach(display => {
+        if (display.className === target.value) {
+            display.hidden = false;
+            return;
+        }
+        display.hidden = true;
+    });
 });
