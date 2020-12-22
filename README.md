8ca2f3d8eaa9efb30627c82d230852652752fb49
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-12-22 15:34:28 -0500

    "Update README ot include overview of conditional form error messages. Refactor validation checks in the keyup event listener. Add focus events for the Activity selections."

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 *Treehouse FullStack JS Techdegree Project 3:*
+
 # Interactive Form
 
 This project demonstrates creating validation and provide interactivity to a form.
+
+## Feature
+
+This project has added real-time error messages for the required fields. This includes as follows:
+
+* Name field
+  * If empty: "Please enter your name."
+  * If only has or begins with spaces: "Your name can not begin or only contain spaces."
+* Email field
+  * If empty: "Please enter your email address."
+  * If spaces or missing characters: "Your email address needs to have the correct format."
+* For the Credit card fields
+  * If empty: "Please enter [input] for the credit card."
+  * If not the correct number of digits or contains other characters: Not a valid credit card [input]
+
+These conditional messages will appear as the user is entering text in the field or on submit.
--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 //   Job Role   //
 //   Section    //
 // ============ //
+
 // Hide "Other" Job Role input on load if JS is turned on
 const otherJobRole = document.querySelector("#other-job-role");
 otherJobRole.hidden = true;
+
 // Grab the Job Title selection menu
 const titleSelection = document.querySelector("#title");
 titleSelection.addEventListener("change", event => {
+
     // If user selects the "other" option only, display the text field
     if (event.target.value !== "other") {
         otherJobRole.hidden = true;
@@ -19,16 +22,21 @@ titleSelection.addEventListener("change", event => {
 //  T-Shirt Info  //
 //    Section     //
 // ============== //
+
 // Grab the Color selection menu
 const colorSelection = document.querySelector("#color");
+
 // Immediately disable it on load; Waits for a design selection
 colorSelection.disabled = true;
+
 // Declare variable to store color options
 const colorOptions = colorSelection.querySelectorAll("option");
+
 // Grab the Design selection menu
 const designSelection = document.querySelector("#design");
 designSelection.addEventListener("change", event => {
     const { target } = event;
+
     // On any change, enable the Color selection menu
     colorSelection.disabled = false;
     colorOptions.forEach(color => {
@@ -42,19 +50,26 @@ designSelection.addEventListener("change", event => {
 //  Register for Activities  //
 //         Section           //
 // ========================= //
+
 // When the Activites selections are checked or unchecked
 document.querySelector("#activities").addEventListener("change", function () {
+
     // Collect the list of activities by their input element
     const activities = this.querySelectorAll(".activities-box input");
+
     // Grab element that displays total cost
     const totalCostDisplay = this.querySelector("#activities-cost");
+
     // Declare a variable to store total cost
     let totalCost = 0;
     activities.forEach((activity) => {
+
         // Quick access to current activity's data attributes
         const { dayAndTime, cost } = activity.dataset;
+
         // Quick access to current activity's main attributes
         const { checked, disabled } = activity;
+
         // Function to change disable an activity depending of inputs with same date/time
         function changeActivityDisable(activities, bool) {
             activities.forEach((similarActivity) => {
@@ -65,11 +80,14 @@ document.querySelector("#activities").addEventListener("change", function () {
             });
         }
         if (checked) {
+
             // If the activity is check, add its cost to the total cost
             totalCost += parseInt(cost);
+
             // Disable activities with same date/time
             changeActivityDisable(activities, true);
         }
+
         // If the current activity is not checked or disabled
         if (!checked && !disabled) {
             // Enable any activities with same date/time
@@ -78,19 +96,23 @@ document.querySelector("#activities").addEventListener("change", function () {
     });
     totalCostDisplay.textContent = `Total: $${totalCost}`;
 });
+
 // ============== //
 //  Payment Info  //
 //     Section    //
 // ============== //
+
 // Grab the Credit Card, Paypal and Bitcoin displays
 const creditcardDisplay = document.querySelector("#credit-card");
 const paypalDisplay = document.querySelector("#paypal");
 const bitcoinDisplay = document.querySelector("#bitcoin");
 const paymentDisplays = [creditcardDisplay, paypalDisplay, bitcoinDisplay];
+
 // Auto Select the "Credit Card" option and hide the PayPal and Bitcoin info on page load
 document.querySelector("option[value='credit-card']").selected = true;
 paypalDisplay.hidden = true;
 bitcoinDisplay.hidden = true;
+
 // Check for changes on the payment method
 document.querySelector("#payment").addEventListener("change", event => {
     const target = event.target;
@@ -102,10 +124,12 @@ document.querySelector("#payment").addEventListener("change", event => {
         display.hidden = true;
     });
 });
+
 // ================ //
 //        Form      //
 //     Validation   //
 // ================ //
+
 // Grab 
 const form = document.querySelector("form");
 const nameInput = document.querySelector("#name");
@@ -117,45 +141,89 @@ const ccInput = {
     ccCVV: document.querySelector("#cvv")
 };
 const { ccNumber, ccZip, ccCVV } = ccInput;
+
 // Function to update error or success styling on validation check
-const validStyle = (containerEl, bool) => {
-    // Store class name based of validation boolean
+const validStyle = (containerEl, bool, errorText = '') => {
     const selectorName = bool ? "valid" : "not-valid";
-    // Store display style of the error hint based on boolean
     const displayStyle = bool ? "none" : "unset";
+
     // Check if a validation class is already attached to activity container
     const classExist = containerEl.classList.contains("valid") || containerEl.classList.contains("not-valid");
+
     // Store the opposite class of the validation bool param
     const prevSelector = !bool ? "valid" : "not-valid";
+
     // Run proper TolkenList method to update validation class if one is already attached or not
     !classExist ? containerEl.classList.add(selectorName) : containerEl.classList.replace(prevSelector, selectorName);
+
     // Provide display style to the error hint element
-    containerEl.lastElementChild.style.display = displayStyle;
+    const { lastElementChild } = containerEl;
+    lastElementChild.style.display = displayStyle;
+    lastElementChild.textContent = errorText;
 };
-// Declare variable that will store returned validation boolean
-let valid;
+const errorDisplayText = (value, regex, noLengthMsg, badStringMsg) => {
+    if (value.length === 0) {
+        return noLengthMsg;
+    }
+    if (!regex.test(value)) {
+        return badStringMsg;
+    }
+};
+
+let isValid;
+
 // Validation checks on keyup in required text fields
 form.addEventListener("keyup", ({ target }) => {
-    // Run validations depending on input typed in
-    if (target === nameInput) {
-        valid = /^\S/g.test(target.value) && target.value !== '';
-    }
-    if (target === emailInput) {
-        valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/i.test(emailInput.value);
-    }
-    if (target === ccNumber) {
-        valid = /^[0-9]{13,16}$/.test(ccNumber.value);
-    }
-    if (target === ccZip) {
-        valid = /^[0-9]{5}$/.test(ccZip.value);
-    }
-    if (target === ccCVV) {
-        valid = /^[0-9]{3}$/.test(ccCVV.value);
-    }
-    // Update validation class for the target's parent element
-    validStyle(target.parentElement, valid);
+
+    const validInputTargets = [
+        {
+            input: nameInput,
+            regex: /^\S/,
+            errorHint: (value, regex) => {
+                return errorDisplayText(value, regex, "Please enter your name.", "Your name can not begin or only contain spaces.");
+            }
+        },
+        {
+            input: emailInput,
+            regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+            errorHint: (value, regex) => {
+                return errorDisplayText(value, regex, "Please enter your email address.", "Your email address needs to have the correct format.");
+            }
+        },
+        {
+            input: ccNumber,
+            regex: /^[0-9]{13,16}$/,
+            errorHint: (value, regex) => {
+                return errorDisplayText(value, regex, "Please Enter a Credit Card Number", "Not a valid Credit Card Number");
+            }
+        },
+        {
+            input: ccZip,
+            regex: /^[0-9]{5}$/,
+            errorHint: (value, regex) => {
+                return errorDisplayText(value, regex, "Please Enter the Zip Code for the Credit Card.", "Not a valid Zip Code.");
+            }
+        },
+        {
+            input: ccCVV,
+            regex: /^[0-9]{3}$/,
+            errorHint: (value, regex) => {
+                return errorDisplayText(value, regex, "Please Enter The Credit Card's Security Code", "Not a Valid Credit Card Security Code.");
+            }
+        }
+    ];
+
+    validInputTargets.forEach(({ input, regex, errorHint }) => {
+        const { value } = input;
+        if (target === input) {
+            isValid = value.length !== 0 && regex.test(value);
+            validStyle(input.parentElement, isValid, errorHint(value, regex));
+        }
+    });
 });
+
 form.addEventListener("submit", event => {
+
     // Store text fields in an array for submit check
     const inputGroup = [
         nameInput,
@@ -164,21 +232,35 @@ form.addEventListener("submit", event => {
         ccZip,
         ccCVV
     ];
+
     // Get array of all activity checkboxes for submit check
     const activitiesGroup = activitiesBox.querySelectorAll("input");
+
     // Show validation error for every text input that is still empty
     inputGroup.forEach((input) => {
         if (input.value === "") {
             validStyle(input.parentElement, false);
-            valid = false;
+            isValid = false;
         }
     });
+
     // Turn nodelist to an array and show validation error if none of the inputs are checked
     if (Array.from(activitiesGroup).every((checkBox) => checkBox.checked === false)) {
         validStyle(activitiesBox.parentElement, false);
-        valid = false;
+        isValid = false;
     }
-    if (!valid) {
+    if (!isValid) {
         event.preventDefault();
     }
+});
+
+const activityCheckboxes = document.querySelectorAll("#activities-box input");
+
+activityCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener("focus", function () {
+        this.parentElement.classList.add("focus");
+    });
+    checkbox.addEventListener("blur", function () {
+        this.parentElement.classList.remove("focus");
+    });
 });
