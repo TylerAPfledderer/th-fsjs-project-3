210917e9426416f8507964ab25d07cfdc67da0b0
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-12-23 19:08:08 -0500

    "Provide a quick formatting update to script.js"

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 //   Job Role   //
 //   Section    //
 // ============ //
+
 // Hide "Other" Job Role input on load if JS is turned on
 const otherJobRole = document.querySelector("#other-job-role");
 otherJobRole.hidden = true;
+
 // Grab the Job Title selection menu
 const titleSelection = document.querySelector("#title");
 titleSelection.addEventListener("change", event => {
+
     // If user selects the "other" option only, display the text field
     if (event.target.value !== "other") {
         otherJobRole.hidden = true;
@@ -15,20 +18,26 @@ titleSelection.addEventListener("change", event => {
     }
     otherJobRole.hidden = false;
 });
+
 // ============== //
 //  T-Shirt Info  //
 //    Section     //
 // ============== //
+
 // Grab the Color selection menu
 const colorSelection = document.querySelector("#color");
+
 // Immediately disable it on load; Waits for a design selection
 colorSelection.disabled = true;
+
 // Declare variable to store color options
 const colorOptions = colorSelection.querySelectorAll("option");
+
 // Grab the Design selection menu
 const designSelection = document.querySelector("#design");
 designSelection.addEventListener("change", event => {
     const { target } = event;
+
     // On any change, enable the Color selection menu
     colorSelection.disabled = false;
     let colorsActive = [];
@@ -42,23 +51,31 @@ designSelection.addEventListener("change", event => {
     }
     colorsActive[0].selected = true;
 });
+
 // ========================= //
 //  Register for Activities  //
 //         Section           //
 // ========================= //
+
 // When the Activites selections are checked or unchecked
 document.querySelector("#activities").addEventListener("change", function () {
+
     // Collect the list of activities by their input element
     const activities = this.querySelectorAll(".activities-box input");
+
     // Grab element that displays total cost
     const totalCostDisplay = this.querySelector("#activities-cost");
+
     // Declare a variable to store total cost
     let totalCost = 0;
     activities.forEach((activity) => {
+
         // Quick access to current activity's data attributes
         const { dayAndTime, cost } = activity.dataset;
+
         // Quick access to current activity's main attributes
         const { checked, disabled } = activity;
+
         // Function to change disable an activity depending of inputs with same date/time
         function changeActivityDisable(activities, bool) {
             activities.forEach((similarActivity) => {
@@ -69,32 +86,40 @@ document.querySelector("#activities").addEventListener("change", function () {
             });
         }
         if (checked) {
+
             // If the activity is check, add its cost to the total cost
             totalCost += parseInt(cost);
+
             // Disable activities with same date/time
             changeActivityDisable(activities, true);
         }
+
         // If the current activity is not checked or disabled
         if (!checked && !disabled) {
+
             // Enable any activities with same date/time
             changeActivityDisable(activities, false);
         }
     });
     totalCostDisplay.textContent = `Total: $${totalCost}`;
 });
+
 // ============== //
 //  Payment Info  //
 //     Section    //
 // ============== //
+
 // Grab the Credit Card, Paypal and Bitcoin displays
 const creditcardDisplay = document.querySelector("#credit-card");
 const paypalDisplay = document.querySelector("#paypal");
 const bitcoinDisplay = document.querySelector("#bitcoin");
 const paymentDisplays = [creditcardDisplay, paypalDisplay, bitcoinDisplay];
+
 // Auto Select the "Credit Card" option and hide the PayPal and Bitcoin info on page load
 document.querySelector("option[value='credit-card']").selected = true;
 paypalDisplay.hidden = true;
 bitcoinDisplay.hidden = true;
+
 // Check for changes on the payment method
 document.querySelector("#payment").addEventListener("change", event => {
     const target = event.target;
@@ -106,10 +131,12 @@ document.querySelector("#payment").addEventListener("change", event => {
         display.hidden = true;
     });
 });
+
 // ================ //
 //        Form      //
 //     Validation   //
 // ================ //
+
 // Grab 
 const form = document.querySelector("form");
 const nameInput = document.querySelector("#name");
@@ -121,21 +148,27 @@ const ccInput = {
     ccCVV: document.querySelector("#cvv")
 };
 const { ccNumber, ccZip, ccCVV } = ccInput;
+
 // Function to update error or success styling on validation check
 const validStyle = (containerEl, bool, errorText = '') => {
     const selectorName = bool ? "valid" : "not-valid";
     const displayStyle = bool ? "none" : "unset";
+
     // Check if a validation class is already attached to activity container
     const classExist = containerEl.classList.contains("valid") || containerEl.classList.contains("not-valid");
+
     // Store the opposite class of the validation bool param
     const prevSelector = !bool ? "valid" : "not-valid";
+
     // Run proper TolkenList method to update validation class if one is already attached or not
     !classExist ? containerEl.classList.add(selectorName) : containerEl.classList.replace(prevSelector, selectorName);
+
     // Provide display style to the error hint element
     const { lastElementChild } = containerEl;
     lastElementChild.style.display = displayStyle;
     lastElementChild.textContent = errorText;
 };
+
 const errorDisplayText = (value, regex, noLengthMsg, badStringMsg) => {
     if (value.length === 0) {
         return noLengthMsg;
@@ -144,7 +177,9 @@ const errorDisplayText = (value, regex, noLengthMsg, badStringMsg) => {
         return badStringMsg;
     }
 };
+
 let isValid;
+
 // Validation checks on keyup in required text fields
 form.addEventListener("keyup", ({ target }) => {
     const validInputTargets = [
@@ -184,6 +219,7 @@ form.addEventListener("keyup", ({ target }) => {
             }
         }
     ];
+
     validInputTargets.forEach(({ input, regex, errorHint }) => {
         const { value } = input;
         if (target === input) {
@@ -192,7 +228,9 @@ form.addEventListener("keyup", ({ target }) => {
         }
     });
 });
+
 form.addEventListener("submit", event => {
+
     // Store text fields in an array for submit check
     const inputGroup = [
         nameInput,
@@ -201,8 +239,10 @@ form.addEventListener("submit", event => {
         ccZip,
         ccCVV
     ];
+
     // Get array of all activity checkboxes for submit check
     const activitiesGroup = activitiesBox.querySelectorAll("input");
+
     // Show validation error for every text input that is still empty
     inputGroup.forEach((input) => {
         if (input.value === "") {
@@ -210,15 +250,18 @@ form.addEventListener("submit", event => {
             isValid = false;
         }
     });
+
     // Turn nodelist to an array and show validation error if none of the inputs are checked
     if (Array.from(activitiesGroup).every((checkBox) => checkBox.checked === false)) {
         validStyle(activitiesBox.parentElement, false);
         isValid = false;
     }
+
     if (!isValid) {
         event.preventDefault();
     }
 });
+
 const activityCheckboxes = document.querySelectorAll("#activities-box input");
 activityCheckboxes.forEach(checkbox => {
     checkbox.addEventListener("focus", function () {
